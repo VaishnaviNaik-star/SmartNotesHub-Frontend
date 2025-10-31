cc13d6254c0a23e749f5fe2009c98ac333c66d64
Vaishnavi Naik <23be0234@sipnaengg.ac.in>
2025-10-31 19:21:25 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -8,10 +8,24 @@ const API = axios.create({
 
 // Attach token automatically
 API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://smartnoteshub-backend.onrender.com/api",
+});
+
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
 export default API;
+
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default API;
+
 
